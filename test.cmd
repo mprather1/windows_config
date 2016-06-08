7001c990d7adb8c78ffb898b78d0734252540e91
Michael Prather <mprather1@gmail.com>
2016-06-07 20:34:23 -0500

    update

--- test.cmd
+++ test.cmd
@@ -1,5 +1,3 @@
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
-cinst firefox googlechrome notepadplusplus.install 7zip.install atom vlc keepass.install ruby putty libreoffice virtualbox chromium thunderbird teamviewer cygwin steam jdk8 nodejs.install classic-shell eclipse php -y
-msiexec -i software\synergy.msi
+cinst firefox googlechrome notepadplusplus.install 7zip.install atom vlc keepass.install ruby putty libreoffice virtualbox chromium thunderbird teamviewer cygwin steam jdk8 foobar2000 nodejs.install classic-shell synergy eclipse php -y
 software\xmousebuttoncontrol.exe
-software\never10.exe
